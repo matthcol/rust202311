a0898f8d8b6e1707449162744330ceddc1448e64
matthcol <matthias.colin@gmail.com>
2023-11-30 17:25:17 +0100

    bonus closure: function as parameter

--- 07-democlosure/src/main.rs
+++ 07-democlosure/src/main.rs
@@ -1,3 +1,26 @@
+
+
+// see also FnMut, FnOnce
+fn play_with_f1<F>(f: F) 
+where
+    F: Fn(u32) -> u32
+{
+    let x = 4;
+    let y = f(x);
+    println!("In play 1: f({x}) = {y}");
+}
+
+fn play_with_f2(f: fn(u32) -> u32) 
+{
+    let x = 4;
+    let y = f(x);
+    println!("In play 2: f({x}) = {y}");
+}
+
+fn compute(x: u32) -> u32 {
+    x*x+1
+}
+
 fn main() {
     let f0 = || 3;    
     let res = f0();
@@ -6,9 +29,13 @@ fn main() {
     let f1 = |x| x + 1; // type of x computed by inference
     let res = f1(4000);
     println!("In main: {res}");
-
+    play_with_f1(f1);
+    play_with_f2(f1);
     // let res = f1(4.4);
     // let res = f1(1u8);
+
+    play_with_f1(compute);
+    play_with_f2(compute);
 
     let f1 = |x: u8| x + 1; 
     let res = f1(40);
